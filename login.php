19418df836e75f56e5d3e5e66e5be3140ead0def
danilohenriqueandrade <danilo.andrade6@etec.sp.gov.br>
2017-05-22 04:57:58 -0300

    Patch
    Removi validação de usuário direto no BD pq ainda não passei as tabelas.

--- login.php
+++ login.php
@@ -1,5 +1,20 @@
 <?php
 
+	$username = $_POST["username"];
+	$password = $_POST["password"];
+
+	// LOGIN OFF DATABASE - REMOVER QUANDO MUDAR PARA PROD
+	if ($username="admin" and $password="admin"){
+		session_start();
+	
+		//Caso contrário redireciona para a página de autenticação
+		$_SESSION['username'] = $username;
+		$_SESSION['password'] = $password;
+
+		header('Location: index.php');
+	}
+	// LOGIN OFF DATABASE - REMOVER QUANDO MUDAR PARA PRODc
+
 	function conectar($host, $usuario, $senha, $bd){
 		mysql_connect($host, $usuario, $senha) or die ("Falha na conexao");
 		mysql_select_db($bd) or die ("Falha no acesso" .$bd);
@@ -7,10 +22,7 @@
 	}
 
 
-	conectar('localhost', 'root', '', 'bd_facebookanalytics');
-
-	$username = $_POST["username"];
-	$password = $_POST["password"];
+	conectar('localhost', 'root', '', 'bd_facebookanalytics');	
 
 	$resultado = mysql_fetch_assoc(mysql_query("SELECT password FROM tb_usuarios WHERE username='".$username."'"));
 
